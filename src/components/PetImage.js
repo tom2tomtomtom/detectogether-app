cbc129934c9f21f369bdf227b6d809c096810ee3
thomas Dowuona-Hyde <tomh@redbaez.com>
2025-08-15 10:36:46 +1000

    ui(pet): implement precise accessory positioning with cropped assets; center math and z-index by type

--- src/components/PetImage.js
+++ src/components/PetImage.js
@@ -20,20 +20,36 @@ const PetImage = ({ mood = 'normal', size = 150, accessory = null, accessories =
     glasses: require('../../assets/sunglasses.png'),
   };
 
-  // Larger sizing/positioning rules for visibility
+  // Positioning with cropped assets (centered math)
   const getAccessoryStyle = (type) => {
     const t = type === 'hat' ? 'tophat' : type === 'glasses' ? 'sunglasses' : type;
+    const base = size * 0.3; // 30% of pet size
     switch (t) {
-      case 'crown':
-        return { width: size * 0.6, height: size * 0.4, top: size * 0.05, left: size * 0.2 };
-      case 'tophat':
-        return { width: size * 0.5, height: size * 0.5, top: 0, left: size * 0.25 };
-      case 'bowtie':
-        return { width: size * 0.4, height: size * 0.3, top: size * 0.5, left: size * 0.3 };
-      case 'sunglasses':
-        return { width: size * 0.5, height: size * 0.25, top: size * 0.3, left: size * 0.25 };
-      default:
-        return { width: size * 0.5, height: size * 0.5, top: size * 0.25, left: size * 0.25 };
+      case 'crown': {
+        const w = base;
+        const h = base * 0.6;
+        return { width: w, height: h, position: 'absolute', top: size * 0.08, left: (size - w) / 2, zIndex: 1 };
+      }
+      case 'tophat': {
+        const w = base * 0.8;
+        const h = base;
+        return { width: w, height: h, position: 'absolute', top: size * 0.02, left: (size - w) / 2, zIndex: 1 };
+      }
+      case 'bowtie': {
+        const w = base * 0.7;
+        const h = base * 0.5;
+        return { width: w, height: h, position: 'absolute', top: size * 0.48, left: (size - w) / 2, zIndex: 2 };
+      }
+      case 'sunglasses': {
+        const w = base;
+        const h = base * 0.4;
+        return { width: w, height: h, position: 'absolute', top: size * 0.28, left: (size - w) / 2, zIndex: 2 };
+      }
+      default: {
+        const w = base;
+        const h = base;
+        return { width: w, height: h, position: 'absolute', top: size * 0.25, left: (size - w) / 2, zIndex: 1 };
+      }
     }
   };
 
@@ -47,13 +63,13 @@ const PetImage = ({ mood = 'normal', size = 150, accessory = null, accessories =
     <View style={[styles.container, { width: size, height: size }]}>
       {/* Back accessories (hats) */}
       {backs.map((a) => (
-        <Image key={`back-${a}`} source={accessoryImages[a]} style={[styles.accessoryImage, getAccessoryStyle(a)]} resizeMode="contain" />
+        <Image key={`back-${a}`} source={accessoryImages[a]} style={getAccessoryStyle(a)} resizeMode="contain" />
       ))}
       {/* Pet */}
-      <Image source={petImages[mood] || petImages.normal} style={[styles.petImage, { width: size, height: size }]} resizeMode="contain" />
+      <Image source={petImages[mood] || petImages.normal} style={styles.petImage} resizeMode="contain" />
       {/* Front accessories */}
       {fronts.map((a) => (
-        <Image key={`front-${a}`} source={accessoryImages[a]} style={[styles.accessoryImage, getAccessoryStyle(a), { zIndex: 2 }]} resizeMode="contain" />
+        <Image key={`front-${a}`} source={accessoryImages[a]} style={getAccessoryStyle(a)} resizeMode="contain" />
       ))}
     </View>
   );
